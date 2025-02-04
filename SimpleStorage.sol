b38403b02ec781ee12d0bb528d68fe6a9fc4039a
Mark-Mulli <markmulli001@gmail.com>
2025-02-04 09:59:37 +0300

    basic solidity concepts

---
+++ SimpleStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    // this will get initialized to 0
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favNum;
+        string name;
+    }
+
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavNum;
+    
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retreive() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson (string memory _name, uint256 _favNum) public {
+        people.push(People({favNum: _favNum, name: _name}));
+        nameToFavNum[_name] = _favNum;
+    }
+   
+}
